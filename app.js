7c696f757412548566fc6501d6ac2f8298e91709
mecdeality <utegen.2019@gmail.com>
2020-12-16 01:05:25 +0600

    some updates

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const axios = require('axios')
 const fs = require('fs');
 const app = express();
 
+//middleware 
+app.use(express.urlencoded({extended: true}));
+
 //connection to MongoDB && models
 const mongoose = require('mongoose')
 const mongoURI = 'mongodb+srv://blog-user-1:test123@node-blog.ggcm8.mongodb.net/node-blog?retryWrites=true&w=majority';
@@ -12,7 +15,9 @@ mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(result => app.listen(3001))
   .catch(err => console.log(err));
 
-const Blog = require('./models/blogs')
+const Blog = require('./models/blogs');
+const { ESRCH } = require('constants');
+const { concat } = require('lodash');
 
 //EJS 
 app.set('view engine', 'ejs');
@@ -25,12 +30,32 @@ app.get('/posts', (req, res) => {
       res.render('posts', {title: 'Posts', posts});
     }).catch(err => console.log(err));
 })
+app.post('/posts', (req, res) => {
+  // console.log(req);
+  const post = new Blog({
+    title: req.body.title,
+    body: req.body.body
+  })
+  post.save()
+    .then(result => res.redirect('/posts'))
+    .catch(err => console.log(err));
+})
+
 
 app.get('/posts/:post_id', (req, res) => {
   Blog.findById(req.params.post_id)
     .then(post => {
       res.render('post', {title: post.id, post });
-    }).catch(err => console.log(err));
+    }).catch(err => {
+      console.log(err, 'afaaffafaaAA');
+      res.status(404).render('404', {title: '404'});
+    });
+})
+app.delete('/posts/:post_id', (req, res) => {
+  console.log('REACHED');
+  Blog.findByIdAndDelete(req.params.post_id)
+    .then(result => res.json('/posts'))
+    .catch(err => console.log(err));
 })
 
 //WITH AXIOS
@@ -65,6 +90,10 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res ) => {
   // res.sendFile('./views/about.html', {root: __dirname});
   res.render('about', {title: 'About'});
+})
+
+app.get('/add-blog', (req, res) => {
+  res.render('add-blog', {title: 'Add Blog'});
 })
 
 app.use((req, res ) => {
